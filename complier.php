67b31a418f794c0f14f40a79b078fb3c535e974f
nishus266 <30533716+nishus266@users.noreply.github.com>
2018-05-08 18:31:56 +0530

    1213

--- complier.php
+++ complier.php
@@ -3,7 +3,13 @@
 <title>compiler</title>
  <meta charset="utf-8">
 
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-2731247551638219",
+    enable_page_level_ads: true
+  });
+</script>  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
    
 <style>
